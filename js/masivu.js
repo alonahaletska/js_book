5d6d9cefbfcc09349e81cffe44a943000d392ee3
alonahaletska <al.khaletskaya@gmail.com>
2023-06-26 00:56:23 +0300

    массив

---
+++ js/masivu.js
@@ -0,0 +1,8 @@
+//создание массива
+var dinosaurs = ['T-Rex', 'Velociraptor', 'Stegosaurus', 'Pterodactel'];
+//выделение одного элемента массива 
+console.log(dinosaurs[0]);//выделиться первый элемент массива (0 потому что счет элементов начинается с 0)
+//изменения элементов в массиве
+// выделяем нужный жлемент и переопределяем через " = "
+console.log(dinosaurs[0] = 'Tyranosavr-Rex');
+console.log(dinosaurs);
